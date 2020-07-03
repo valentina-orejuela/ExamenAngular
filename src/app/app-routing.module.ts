836cd6c33806e3c82436846445c510da683e47c2
Valentina Orejuela <valentina.orejuela_a@uao.edu.co>
2020-07-03 13:46:34 -0500

    Se trabajo en el menu lateral o sideBar y en la navBar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'pagina-principal', pathMatch: 'full'
+  },
+  {
+    path: 'pagina-principal', component: PaginaPrincipalComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
