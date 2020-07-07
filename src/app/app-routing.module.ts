3273d4e632e00a9359a3865fc3648ccfd7dab240
Valentina Orejuela <valentina.orejuela_a@uao.edu.co>
2020-07-06 22:50:36 -0500

    Se trabajo en la maquetación de la pagina principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
 
 
 const routes: Routes = [
-  {
-    path: '', redirectTo: 'pagina-principal', pathMatch: 'full'
-  },
-  {
-    path: 'pagina-principal', component: PaginaPrincipalComponent
-  },
+  
 ];
 
 @NgModule({
